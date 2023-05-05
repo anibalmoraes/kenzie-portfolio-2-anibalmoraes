4f45c06d8c3a158f46289b07261fb2c3b190c37a
Anibal <anibal4g@gmail.com>
2023-05-05 03:24:28 -0300

    feat: changes

--- src/utils/userData.ts
+++ src/utils/userData.ts
@@ -1,9 +1,9 @@
 export const userData = {
-  nameUser: "Samuel Leão",
-  githubUser: "samuelleaokenzie",
-  linkedinUser: "nekelpatrick",
-  whatsappNumber: "+5541988819028",
-  emailUser: "samuel@kenzie.com.br",
+  nameUser: "Anibal Farias",
+  githubUser: "anibalmoraes",
+  linkedinUser: "anibal-farias-28a25a163",
+  whatsappNumber: "+5591988597684",
+  emailUser: "anibal4g@gmail.com",
   instagramUser: "samuelleaoui",
   facebookUser: "",
 };
